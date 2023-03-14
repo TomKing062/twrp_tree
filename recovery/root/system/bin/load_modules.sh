031f8bc4974632ca7458a62cb9ea1121d899be62
TomKing062 <yx42_44@126.com>
2023-03-14 22:11:34 +0800

    more firmware

--- recovery/root/system/bin/load_modules.sh
+++ recovery/root/system/bin/load_modules.sh
@@ -2,14 +2,19 @@
 
 load()
 {
+        mkdir /s
         mkdir /v
-        mount -t ext4 -o ro /dev/block/by-name/socko /v
+        mount -t ext4 -o ro /dev/block/by-name/socko /s
+        mount -t ext4 -o ro /dev/block/by-name/vendor /v
         mkdir -p /vendor/lib/modules
-        cp /v/synaptics_dsx_td4310.ko /vendor/lib/modules/synaptics_dsx_td4310.ko
-        cp /v/synaptics_td4320_spi_ts.ko /vendor/lib/modules/synaptics_td4320_spi_ts.ko
+        cp /s/*.ko /vendor/lib/modules/
+        cp -a /v/firmware /vendor/
+        wait 1
         insmod /vendor/lib/modules/synaptics_dsx_td4310.ko
         insmod /vendor/lib/modules/synaptics_td4320_spi_ts.ko
+        umount /s
         umount /v
+        rmdir /s
         rmdir /v
 }
 

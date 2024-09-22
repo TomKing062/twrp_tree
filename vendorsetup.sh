8bd560ce5fc7f5809bb5b4692dbcc521ccaff4dd
TomKing062 <yx42_44@126.com>
2024-09-22 12:28:27 +0800

    remove py patch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,4 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-patch -Np1 < device/philips/S701/check_radio_versions.py.patch
-patch -Np1 < device/philips/S701/post_process_props.py.patch
-
 add_lunch_combo omni_S701-eng
